79ae438ef319d50ddcd42431b96ed66802452be4
Andrew Yourtchenko <ayourtch@gmail.com>
2014-11-07 17:27:30 +0000

    move the stateless generate/verify portion into separate files

---
+++ ipcookies_stateless.c
@@ -0,0 +1,29 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#define __APPLE_USE_RFC_3542
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/poll.h>
+#include <time.h>
+#include <netinet/icmp6.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+#include "ipcookies.h"
+
+int ipcookie_verify_stateless(ipcookie_t testcookie, struct in6_addr *src) {
+  /* FIXME */
+  return 0;
+}
+
+void ipcookie_set_stateless(ipcookie_t *target_cookie, struct in6_addr *peer) {
+  /* FIXME */
+}
+
---
+++ ipcookies_stateless.h
@@ -0,0 +1,16 @@
+enum {
+  IPCOOKIE_NOMATCH = 0,
+  IPCOOKIE_MATCH_PREV,
+  IPCOOKIE_MATCH_CURR
+} ipcookie_match_enum_t;
+
+/*
+ *  ipcookie_verify_stateless returns:
+ *     IPCOOKIE_NOMATCH: not matched
+ *     IPCOOKIE_MATCH_PREV: matched the previous cookie
+ *     IPCOOKIE_MATCH_CURR: matched the current cookie
+ */
+
+int ipcookie_verify_stateless(ipcookie_t testcookie, struct in6_addr *src);
+
+void ipcookie_set_stateless(ipcookie_t *target_cookie, struct in6_addr *peer);
